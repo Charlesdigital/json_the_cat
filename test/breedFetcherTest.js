5e25d2c054330595cd0f169008c5b372fb04ecd4
Charlesdigital <charles.easton123@gmail.com>
2021-09-25 17:41:26 -0400

    cat all done with tests

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -5,6 +5,7 @@ const { assert } = require("chai");
 
 describe("fetchBreedDescription", () => {
   it("returns a string description for a valid breed, via callback", (done) => {
+    //this is happy path for, to check if things are working properly
     fetchBreedDescription("Siberian", (err, desc) => {
       // we expect no error for this scenario
       assert.equal(err, null);
@@ -18,16 +19,16 @@ describe("fetchBreedDescription", () => {
       done();
     });
   });
-  it("returns a string description for a valid breed, via callback", (done) => {
-    fetchBreedDescription("Siberian", (err, desc) => {
-      // we expect no error for this scenario
-      assert.equal(err, null);
+  it("we expect the first argument for our callback (err) to be set, and desc to be null.", (done) => {
+    const error = new Error("Test"); //initiate an error type
+    fetchBreedDescription(error, (err, desc) => {
+      // we error for this scenario
+      assert.notEqual(err, null);
 
-      const expectedDesc =
-        "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+      const expectedDesc = null;
 
       // compare returned description
-      assert.equal(expectedDesc, desc.trim());
+      assert.equal(expectedDesc, null);
 
       done();
     });
